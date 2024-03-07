42f5509c37bd09ba16bb737af4526971014b63ac
Anton Listkov <antonlistkov@gmail.com>
2024-03-07 23:38:59 +0100

    feat: create TypeOrm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { TodosModule } from './todos/todos.module';
 import configuration from './config/configuration';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       load: [configuration],
     }),
+    TypeOrmModule.forRootAsync(typeOrmConfig),
     TodosModule,
   ],
   controllers: [AppController],
---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,24 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import {
+  TypeOrmModuleAsyncOptions,
+  TypeOrmModuleOptions,
+} from '@nestjs/typeorm';
+
+export const typeOrmConfig: TypeOrmModuleAsyncOptions = {
+  imports: [ConfigModule],
+  inject: [ConfigService],
+
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => ({
+    type: 'postgres',
+    host: configService.get<string>('DATABASE_HOST'),
+    port: configService.get<number>('DATABASE_PORT'),
+    username: configService.get<string>('DATABASE_USER'),
+    password: configService.get<string>('DATABASE_PASSWORD'),
+    database: configService.get<string>('DATABASE_NAME'),
+    synchronize: true,
+    entities: [],
+    migrations: [],
+  }),
+};
