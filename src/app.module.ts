6930b95013d87e003fffc6f4426994d7ce6805c3
Anton Listkov <antonlistkov@gmail.com>
2024-03-17 13:54:31 +0100

    refactor: remove unnecessary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
 import { TodosModule } from './todos/todos.module';
 import configuration from './config/configuration';
 import { typeOrmConfig } from './config/typeorm.config';
@@ -19,7 +16,7 @@ import { typeOrmConfig } from './config/typeorm.config';
     TypeOrmModule.forRootAsync(typeOrmConfig),
     TodosModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
