17f927ead776a7c60bb134768e91d185a785a189
Anton Listkov <antonlistkov@gmail.com>
2024-03-18 10:42:05 +0100

    feat: save user in db

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -3,8 +3,11 @@ import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
 import { BcryptService } from './hashing/bcrypt.service';
 import { HashingService } from './hashing/hashing.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthenticationController],
   providers: [
     AuthenticationService,
--- src/authentication/authentication.service.ts
+++ src/authentication/authentication.service.ts
@@ -1,14 +1,29 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
 import { SignInDto } from './dto/sign-in.dto';
 import { SignUpDto } from './dto/sign-up.dto';
+import { User } from 'src/users/entities/user.entity';
+import { HashingService } from './hashing/hashing.service';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class AuthenticationService {
-  async signIn(signInDto: SignInDto) {
-    console.log(signInDto);
+  constructor(
+    private readonly hashingService: HashingService,
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+  async signUp(signUpDto: SignUpDto) {
+    try {
+      const user = new User();
+      user.email = signUpDto.email;
+      user.password = await this.hashingService.hash(signUpDto.email);
+      await this.userRepository.save(user);
+    } catch (err) {
+      throw new ConflictException('We have user with same email');
+    }
   }
 
-  async signUp(signUpDto: SignUpDto) {
-    console.log(signUpDto);
+  async signIn(signInDto: SignInDto) {
+    console.log(signInDto);
   }
 }
