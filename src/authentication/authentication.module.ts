6e9b1d1a59d3e96ea644fb5b5d27ada763b7a19f
Anton Listkov <antonlistkov@gmail.com>
2024-03-18 10:34:33 +0100

    feat: add hashing service

--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { AuthenticationController } from './authentication.controller';
+import { BcryptService } from './hashing/bcrypt.service';
+import { HashingService } from './hashing/hashing.service';
 
 @Module({
   controllers: [AuthenticationController],
-  providers: [AuthenticationService],
+  providers: [
+    AuthenticationService,
+    {
+      provide: HashingService,
+      useClass: BcryptService,
+    },
+  ],
 })
 export class AuthenticationModule {}
