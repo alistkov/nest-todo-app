db30e4ea0a51e98508c2f73ee6a9e8cbe3b1e762
Anton Listkov <antonlistkov@gmail.com>
2024-03-20 22:14:44 +0100

    feat: add password regex

--- src/authentication/dto/sign-up.dto.ts
+++ src/authentication/dto/sign-up.dto.ts
@@ -1,9 +1,14 @@
-import { IsEmail, Length } from 'class-validator';
+import { IsEmail, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class SignUpDto {
   @IsEmail()
   email: string;
 
-  @Length(8, 16)
+  @MinLength(8)
+  @MaxLength(16)
+  @Matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/, {
+    message:
+      'At least 1 capital letter, 1 lowercase letter, 1 special character, 1 numeric character',
+  })
   password: string;
 }
